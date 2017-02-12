3d57e3c184be99e6efda02467ff007d234c6ed93
Yangdm <yangdm0209@outlook.com>
2017-02-12 20:47:55 +0800

    add table javascript

---
+++ static/purchase/new.js
@@ -0,0 +1,12 @@
+$('.editable-num').editable({
+    type: 'text',
+    mode: 'inline',
+    url: '',
+    title: '修改数量'
+});
+$('.editable-price').editable({
+    type: 'text',
+    mode: 'inline',
+    url: '',
+    title: '修改价格'
+});
