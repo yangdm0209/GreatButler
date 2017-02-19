c707c3fe11389bd78436d01d757196f2167f2664
daming <yangdaming@dashuqinzi.com>
2017-02-19 22:39:23 +0800

    allocate

--- web/web_stock.py
+++ web/web_stock.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
-from stock.models import Stock
+from product.models import Product
+from stock.models import Stock, Allocate, AllocateNum
+from utils.response import failed_response, success_response
 
 
 @login_required
@@ -12,9 +17,30 @@ def stock(request):
     return render_to_response('stock/main.html', RequestContext(request, {'stock_active': 1}))
 
 
-@login_required
+@csrf_exempt
+# @login_required
 def allocate(request):
-    return render_to_response('stock/allocate.html', RequestContext(request, {'stock_active': 1}))
+    if request.method == 'GET':
+        stocks = Stock.objects.all()
+        return render_to_response('stock/allocate.html', RequestContext(request, {'stock_active': 1, 'stocks': stocks}))
+    else:
+        data = request.POST.get('data')
+        if not data:
+            return failed_response('参数错误')
+        else:
+            all = json.loads(data)
+            al = Allocate()
+            al.source_stock = Stock.objects.get(id=all['source_stock'])
+            al.dest_stock = Stock.objects.get(id=all['dest_stock'])
+            al.save()
+            for item in all['detail']:
+                pro = AllocateNum()
+                pro.product = Product.objects.get(id=item['pid'])
+                pro.num = int(item['pnum'])
+                pro.save()
+                al.nums.add(pro)
+            al.save()
+            return success_response('调拨成功')
 
 
 @login_required
